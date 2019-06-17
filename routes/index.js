c334149259d6aa95d933488b74905deb8cf3de2c
clairecasalnova <ccc2876@rit.edu>
2019-06-16 22:41:55 -0600

    halfway done with academics page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Express' });
 });
 
+/* GET academics page. */
+router.get('/academics', function (req, res) {
+  res.render('academics', { title: 'Academics' });
+});
+
+exports.index = function (req, res) {
+  res.render('academics', { title: 'Academics' });
+};
+
+/* GET athletics page. */
+router.get('/athletics', function (req, res) {
+  res.render('athletics', { title: 'Athletics' });
+});
+
+exports.index = function (req, res) {
+  res.render('athletics', { title: 'Athletics' });
+};
+
+/* GET experience page. */
+router.get('/experience', function (req, res) {
+  res.render('experience', { title: 'Experience' });
+});
+
+exports.index = function (req, res) {
+  res.render('experience', { title: 'Experience' });
+};
+
 
 /* GET blog page. */
 router.get('/blog', function (req, res) {
