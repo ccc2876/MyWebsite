8f9fba5c9aa830bbcb3ea1e837a5b77aa5a4d33e
clairecasalnova <ccc2876@rit.edu>
2019-06-16 21:02:51 -0600

    finalized blog page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Express' });
 });
 
+
+/* GET blog page. */
+router.get('/blog', function (req, res) {
+  res.render('blog', { title: 'Blog' });
+});
+
+exports.index = function (req, res) {
+  res.render('blog', { title: 'Blog' });
+};
+
 module.exports = router;
